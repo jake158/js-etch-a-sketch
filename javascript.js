cc58dd7c6baefdbabc4a453982e4a9499f16e1c1
freeram <kulikovpavel2801@gmail.com>
2024-05-13 20:02:35 -0600

    Add changing grid size, rainbow mode

--- javascript.js
+++ javascript.js
@@ -3,18 +3,51 @@ const board = document.querySelector('#board');
 const boardSize = 900;
 board.setAttribute('style', `width: ${boardSize}px; height: ${boardSize}px;`);
 
+const button = document.querySelector('button');
+button.addEventListener('click', changeGridSize);
+
+
 function createGrid(size) {
     const boxSize = boardSize / size;
     for (let i = 0; i < Math.pow(size, 2); i++) {
         let box = document.createElement('div');
         box.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px;`);
-        box.addEventListener('mouseover', onHover);
+        box.addEventListener('mouseover', onBoxHover);
         board.appendChild(box);
     }
 }
 
-function onHover(event) {
-    event.target.style.backgroundColor = "gray";
+function clearGrid() {
+    board.innerHTML = '';
+}
+
+let currentColorIndex = 0;
+function rainbowColor() {
+    const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+    const color = colors[currentColorIndex];
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    return color;
+}
+
+function onBoxHover(event) {
+    const rainbowMode = document.querySelector('input');
+    if (rainbowMode.checked) {
+        event.target.style.backgroundColor = rainbowColor();
+    }
+    else {
+        event.target.style.backgroundColor = "black";
+    }
+}
+
+function changeGridSize() {
+    let newSize = prompt("Enter size between 1 and 100:");
+    if (newSize != null && newSize >= 1 && newSize <= 100) {
+        clearGrid();
+        createGrid(newSize);
+    }
+    else {
+        alert("Invalid value");
+    }
 }
 
 createGrid(32);
